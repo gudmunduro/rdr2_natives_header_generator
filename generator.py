c8142b3d5b340cfa83e074aa98902cfa7173e3c5
= <goh00@protonmail.com>
2019-11-10 18:56:52 +0000

    More readable code

--- generator.py
+++ generator.py
@@ -1,73 +1,95 @@
 import json
 from datetime import date
 
-gta5Natives = json.load(open('gta5natives.json'))
-rdr2Natives = json.load(open('rdr2natives.json'))
-gta5NativesUngrouped = {}
+gta5_natives = json.load(open('gta5natives.json'))
+rdr2_natives = json.load(open('rdr2natives.json'))
+gta5_natives_ungrouped = {}
 
 
-def gta5_params_if_compatible(nativeData):
-    global gta5NativesUngrouped
+def gta5_return_type_if_compatible(native_data):
+    """Returns the GTA 5 return type if there is a native with the same name from the GTA 5 native db"""
+    global gta5_natives_ungrouped
 
-    if (nativeData['name'] in gta5NativesUngrouped and 
-            len(nativeData['params']) >= len(gta5NativesUngrouped[nativeData['name']]['params']) and 
-            'params' in gta5NativesUngrouped[nativeData['name']]):
-        params = gta5NativesUngrouped[nativeData['name']]['params']
-        if len(params) < len(nativeData['params']):
+    if (native_data['name'] in gta5_natives_ungrouped and
+            'results' in gta5_natives_ungrouped[native_data['name']]):
+        return gta5_natives_ungrouped[native_data['name']]['results']
+
+    return native_data['return_type']
+
+
+def gta5_params_if_compatible(native_data):
+    """
+    Return the parameters from native db for GTA 5 if it has as many of more parameters
+    (if there are more, the remaing ones will be from the RDR 2 db)
+    """
+    global gta5_natives_ungrouped
+
+    if (native_data['name'] in gta5_natives_ungrouped and
+            len(native_data['params']) >= len(gta5_natives_ungrouped[native_data['name']]['params']) and
+            'params' in gta5_natives_ungrouped[native_data['name']]):
+        params = gta5_natives_ungrouped[native_data['name']]['params']
+        if len(params) < len(native_data['params']):
             missingParamStartIndex = len(params)
             print(f"Missing params start at {missingParamStartIndex}")
-            params += nativeData['params'][missingParamStartIndex:]
+            params += native_data['params'][missingParamStartIndex:]
         return params
 
-    return nativeData['params']
+    return native_data['params']
 
-def gta5_return_type_if_compatible(nativeData):
-    global gta5NativesUngrouped
 
-    if (nativeData['name'] in gta5NativesUngrouped and
-            'results' in gta5NativesUngrouped[nativeData['name']]):
-        return gta5NativesUngrouped[nativeData['name']]['results']
+def param_texts(return_type, params):
+    """
+    Generates:
+    parameters to be used as parameters of the function
+    parameter types to be used as template type parameters
+    parameter names to be used as parameters in Native::Invoke
+    and returns it as a tuple
+    """
+    parameter_text = ''
+    parameter_type_text = return_type
+    parameter_name_text = ''
+    for param in params:
+        if params.index(param) == 0:
+            parameter_name_text += ', '  # The first one is the hash so the comman needs to be added after that
+            parameter_type_text += ', '  # The first one is the return type so the comman needs to be added after that
+  
+        parameter_text += f'{param["type"]} {param["name"]}'
+        parameter_type_text += param["type"]
+        parameter_name_text += param["name"]
+        if params.index(param) < len(params) - 1:
+            parameter_text += ", "
+            parameter_type_text += ', '
+            parameter_name_text += ', '
 
-    return nativeData['return_type']
+    return parameter_text, parameter_type_text, parameter_name_text
+
 
 # Merge all natives for GTA 5 into one dictionary with the name as the key
-for nativeGroupName, nativeGroup in gta5Natives.items():
-    namedNativeGroup = {}
-    for nativeHash, nativeData in nativeGroup.items():
-        namedNativeGroup[nativeData['name']] = nativeData
-    gta5NativesUngrouped.update(namedNativeGroup)
+for native_group_name, native_group in gta5_natives.items():
+    named_native_group = {}
+    for native_hash, native_data in native_group.items():
+        named_native_group[native_data['name']] = native_data
+    gta5_natives_ungrouped.update(named_native_group)
 
-dateStr = date.today().strftime("%d/%m/%Y")
 
 # Generate header file
-headerFileText = f'// Generated {dateStr} \n\n'
+dateStr = date.today().strftime("%d/%m/%Y")
+header_file_text = f'// Generated {dateStr} \n\n'
 
-for nativeGroupName, nativeGroup in rdr2Natives.items():
-    headerFileText += f'namespace {nativeGroupName} \n{{\n'
-    for native, nativeData in nativeGroup.items():
-        return_type = gta5_return_type_if_compatible(nativeData)
-        params = gta5_params_if_compatible(nativeData)
+for nativeGroupName, nativeGroup in rdr2_natives.items():
+    header_file_text += f'namespace {nativeGroupName} \n{{\n'  # Start of namespace
+    for native_hash, native_data in nativeGroup.items():
+        return_type = gta5_return_type_if_compatible(native_data)
+        params = gta5_params_if_compatible(native_data)
 
-        parameterText = ''
-        parameterTypeText = return_type
-        parameterNameText = ''
-        for param in params:
-            if params.index(param) == 0:
-                parameterNameText += ', '
-                parameterTypeText += ', '
-            
-            parameterText += f'{param["type"]} {param["name"]}'
-            parameterTypeText += param["type"]
-            parameterNameText += param["name"]
-            if params.index(param) < len(params) - 1:
-                parameterText += ", "
-                parameterTypeText += ', '
-                parameterNameText += ', '
+        parameter_text, parameter_type_text, parameter_name_text = param_texts(return_type, params)
 
-        headerFileText += f'    static {return_type} {nativeData["name"]}({parameterText}) {{ return Native::Invoke<{parameterTypeText}>({native}{parameterNameText}); }} \n'
-    headerFileText += '}\n'
+        # Native function
+        header_file_text += f'    static {return_type} {native_data["name"]}({parameter_text}) {{ return Native::Invoke<{parameter_type_text}>({native_hash}{parameter_name_text}); }} \n'
+    header_file_text += '}\n'  # End of namespace
+
 
 # Write to header file
-headerFile = open("natives.h", 'w')
-headerFile.write(headerFileText)
-headerFile.close()
+header_file = open("natives.h", 'w')
+header_file.write(header_file_text)
+header_file.close()
